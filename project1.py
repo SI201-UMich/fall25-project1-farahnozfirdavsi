308051331fb692c9c070d8e826497b5f673a3038
Farahe <farahfirdavsi@Farahs-MacBook-Air-646.local>
2025-10-15 20:40:34 -0400

    farahs first function, avg body mass by species & sesex

--- project1.py
+++ project1.py
@@ -9,3 +9,30 @@ def load_data(penguins.csv):
         for row in csv_reader:
             data.append(row)
     return data
+
+# helping safe function to convert strings into floats
+# so that we could calculate better
+def safe_float(value):
+    try:
+        val = float(value)
+        return val
+    except (ValueError, TypeError):
+        return None
+    
+# Farah's functions
+def avg_body_mass_by_species_and_sex(data):
+    results = {}
+    for row in data:
+        species = row.get("species", "")
+        sex = row.get("sex", "")
+        body_mass = safe_float(row.get("body_mass_g", ""))
+        if not in species or not sex or body_mass is None:
+            continue
+        key = (species, sex)
+        if key not in results:
+            results[key] = {"total": 0, "count": 0}
+        results[key]["total"] += body_mass
+        results[key]["count"] += 1
+    avg = {f"{sp} {{sx}}": roung(v["total"] / v["count"], 2) 
+           for (sp, sx), v in results.items()}
+    return avg
